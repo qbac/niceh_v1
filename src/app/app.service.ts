470d9470edcb588911e5d2f9d089e4068dd971aa
qbac <jakub.cyrklaf@gmail.com>
2018-04-23 22:39:11 +0200

    cosmetic corrections

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@angular/core";
-import { Http, Jsonp, Response } from "@angular/http";
-import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/catch";
-import "rxjs/add/operator/map";
+import { Injectable } from '@angular/core';
+import { Http, Jsonp, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AppService {
